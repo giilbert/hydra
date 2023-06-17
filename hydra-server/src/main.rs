e83bee84901371bd4f782de389f571c33832d70f
Gilbert Zhang <gilbertzhang1208@gmail.com>
2023-06-16 22:04:07 -0400

    production port stuff

--- hydra-server/src/main.rs
+++ hydra-server/src/main.rs
@@ -65,25 +65,24 @@ async fn main() -> anyhow::Result<()> {
                 .allow_methods(Any),
         );
 
-    let ports = Ports {
-        http: std::env::var("PORT")
-            .ok()
-            .and_then(|port| port.parse().ok())
-            .unwrap_or(8080),
-        https: std::env::var("PORT_HTTPS")
-            .ok()
-            .and_then(|port| port.parse().ok())
-            .unwrap_or(8443),
-    };
-
     let config = create_rustls_config().await;
 
+    // run if it is not a production environment
+    if std::env::var("ENVIRONMENT").unwrap_or_else(|_| "development".to_string()) != "production" {
+        app_without_https_redirect(router.clone(), state.clone()).await;
+        return Ok(());
+    }
+
+    let ports = Ports {
+        http: 80,
+        https: 443,
+    };
     tokio::spawn(redirect_http_to_https(ports));
 
     let addr = SocketAddr::from(([0, 0, 0, 0], ports.https));
     log::info!("HTTPS listening on {}", addr);
     axum_server::bind_rustls(addr, config)
-        .serve(router.into_make_service())
+        .serve(router.with_state(state).into_make_service())
         .await
         .unwrap();
 
@@ -137,9 +136,18 @@ async fn redirect_http_to_https(ports: Ports) {
     };
 
     let addr = SocketAddr::from(([0, 0, 0, 0], ports.http));
-    log::info!("HTTP listening on {}", addr);
+    log::info!("HTTP -> HTTPS redirect listening on {}", addr);
     axum::Server::bind(&addr.into())
         .serve(redirect.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn app_without_https_redirect(router: Router<AppState>, state: AppState) {
+    let addr = SocketAddr::from(([0, 0, 0, 0], 3001));
+    log::info!("Plain HTTP listening on {}", addr);
+    axum::Server::bind(&addr.into())
+        .serve(router.with_state(state).into_make_service())
         .await
         .unwrap();
 }
