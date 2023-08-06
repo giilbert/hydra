35337e9233b8232f386a7f2d9cc2a15689fdb97c
Gilbert Zhang <gilbertzhang1208@gmail.com>
2023-08-06 14:44:11 -0400

    fix: add shared crate to cache calculation

--- scripts/build/daemon.py
+++ scripts/build/daemon.py
@@ -2,7 +2,7 @@ from utils import cmd, bail
 import cache
 
 
-@cache.fs_cache("hydrad", ["deploy/hydrad.dockerfile", "crates/hydrad"])
+@cache.fs_cache("hydrad", ["deploy/hydrad.dockerfile", "crates/hydrad", "crates/shared"])
 def build():
     if cmd("docker build --file ./deploy/hydrad.dockerfile -t hydrad .") != 0:
         bail("docker build exited with a non-zero exit code.")
--- scripts/build/server.py
+++ scripts/build/server.py
@@ -2,7 +2,7 @@ from utils import cmd, bail
 import cache
 
 
-@cache.fs_cache("hydra-server", ["deploy/server.dockerfile", "crates/hydra-server"])
+@cache.fs_cache("hydra-server", ["deploy/server.dockerfile", "crates/hydra-server", "crates/shared"])
 def build():
     if cmd("docker build --file ./deploy/server.dockerfile -t hydra-server .") != 0:
         bail("docker build exited with a non-zero exit code.")
