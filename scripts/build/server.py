50341e4a7ffe1d05da6156e20f264112250b4362
Gilbert Zhang <gilbertzhang1208@gmail.com>
2023-08-07 11:06:45 -0400

    fix: add dockerfiles to server cache dependencies

--- scripts/build/server.py
+++ scripts/build/server.py
@@ -2,7 +2,15 @@ from utils import cmd, bail
 import cache
 
 
-@cache.fs_cache("hydra-server", ["deploy/server.dockerfile", "crates/hydra-server", "crates/shared"])
+@cache.fs_cache(
+    "hydra-server",
+    [
+        "deploy/server.dockerfile",
+        "crates/hydra-server",
+        "crates/shared",
+        "deploy",
+    ],
+)
 def build():
     if cmd("docker build --file ./deploy/server.dockerfile -t hydra-server .") != 0:
         bail("docker build exited with a non-zero exit code.")
